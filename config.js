e940951c9bc3dd94abd54380d37b835abf97aff0
andigagaga <andigagaga1@gmail.com>
2025-07-12 19:48:44 +0700

    Update new

--- config.js
+++ config.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const mongoURI =
-      process.env.NODE_ENV === "production"
-        ? process.env.MONGO_URI_SERVER
-        : process.env.MONGO_URI_LOCAL;
+    const mongoURI = process.env.MONGO_URI;
 
     const conn = await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
